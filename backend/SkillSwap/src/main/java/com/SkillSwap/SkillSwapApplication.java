0fa38c48056dd3cedf13139f8e11ec21f5af38b7
Aayushdubey05 <dubeyaayush333@gmail.com>
2025-07-12 12:12:08 +0530

    Added the new status

--- backend/SkillSwap/src/main/java/com/SkillSwap/SkillSwapApplication.java
+++ backend/SkillSwap/src/main/java/com/SkillSwap/SkillSwapApplication.java
@@ -2,9 +2,10 @@ package com.SkillSwap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-public class SkillSwapApplication {
+public class SkillSwapApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SkillSwapApplication.class, args);
